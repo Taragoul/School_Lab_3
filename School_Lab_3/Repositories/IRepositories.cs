68ababe28e5289ff100017f0498014ce6e93ec0d
Zak Troy <zak.troy@chasacademy.se>
2024-12-15 18:46:18 +0100

    Added Repository Interface for cleanliness

---
+++ School_Lab_3/Repositories/IRepositories.cs
@@ -0,0 +1,11 @@
+﻿namespace School_Lab_3.Repositories
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int id);
+        Task AddAsync(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
